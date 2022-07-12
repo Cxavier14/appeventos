faf729b611fa2a009abcadd375e9e4f1e4daec10
Eduardo Xavier <eduardoxavi15@gmail.com>
2022-07-12 16:20:56 -0300

    iniciando back end

---
+++ Back/src/AppEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AppEventos.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public IEnumerable<Evento> _evento = new Evento[] {
+                new Evento() {
+                EventoId = 1,
+                Tema = "Angular 11",
+                Local = "Blumenau",
+                Lote = "1º lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                ImagemURL = "Foto.png"
+                },
+                new Evento() {
+                EventoId = 2,
+                Tema = ".NET 5",
+                Local = "Brusque",
+                Lote = "2º lote",
+                QtdPessoas = 350,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                ImagemURL = "Foto1.png"
+                }
+            };
+
+        public EventoController()
+        {
+
+        }
+
+    [HttpGet]
+    public IEnumerable<Evento> Get()
+    {
+        return _evento;
+    }
+
+    [HttpGet("{id}")]
+    public IEnumerable<Evento> GetById(int id)
+    {
+        return _evento.Where(x => x.EventoId == id);
+    }
+
+    [HttpPost]
+    public string Post()
+    {
+        return "Exemplo de post";
+    }
+
+    [HttpPut("{id}")]
+    public string Put(int id)
+    {
+        return $"Exemplo de put com id = {id}";
+    }
+
+    [HttpDelete("{id}")]
+    public string Delete(int id)
+    {
+        return $"Exemplo de delete com id = {id}";
+    }
+}
+}
