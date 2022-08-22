3f3e7c8cdc767c690dd9267e3f9c3d4f4f15efc1
Eduardo Xavier <eduardoxavi15@gmail.com>
2022-08-22 19:12:57 -0300

    adicionando busca

--- Front/AppEventos-App/src/app/eventos/eventos.component.ts
+++ Front/AppEventos-App/src/app/eventos/eventos.component.ts
@@ -9,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 export class EventosComponent implements OnInit {
 
   public eventos: any = [];
+  public eventosFiltrados: any = [];
+
   widthImg: number = 150;
   marginImg: number = 2;
   showImg: boolean = true;
+  private _listFilter: string = "";
+
+  public get listFilter(): string {
+    return this._listFilter;
+  }
+
+  public set listFilter(value: string){
+    this._listFilter = value;
+    this.eventosFiltrados = this.listFilter ? this.eventFilter(this.listFilter) : this.eventos;
+  }
+
+  eventFilter(filterFor: string): any {
+    filterFor = filterFor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: { tema: string; local : string}) => evento.tema.toLocaleLowerCase().indexOf(filterFor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filterFor) !== -1
+    )
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -25,7 +45,10 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void {
     this.http.get('https://localhost:44365/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = response;
+      },
       error => console.log(error)
     );
   }
